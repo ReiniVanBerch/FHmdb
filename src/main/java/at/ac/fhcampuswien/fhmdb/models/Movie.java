7bf987dddbfef473e8d3814fd4fb20857fce5045
Florian Koller <koller.florian@gmail.com>
2025-02-14 20:53:14 +0100

    intialize movies + test

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,16 +1,18 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Movie {
     private String title;
     private String description;
     // TODO add more properties here
-
-    public Movie(String title, String description) {
+    private List<Genre> genres;
+    public Movie(String title, String description, List<Genre> genres) {
         this.title = title;
         this.description = description;
+        this.genres =genres;
     }
 
     public String getTitle() {
@@ -21,10 +23,14 @@ public class Movie {
         return description;
     }
 
+    public List<Genre> getGenres(){ return genres; }
+
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
         // TODO add some dummy data here
-
+        movies.add(new Movie("Der Soldat Jamey Ryan", "Eine Gruppe an Soldaten rettet einen einzelnen Soldaten", List.of(Genre.WAR)));
+        movies.add(new Movie("Schneewittchen","Sieben einsame kleinwüchsige Menschen finden ein Waisenkind", List.of(Genre.DOCUMENTARY)));
+        movies.add(new Movie("New Kids Turbo","verrückter Mongojunge, Busfahrer",List.of(Genre.HISTORY)));
         return movies;
     }
 }
--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -1,7 +1,22 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import jdk.jfr.Description;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HomeControllerTest {
+    @Test
+    @DisplayName("Testet den Initializier")
+    public void test_the_nitializer(){
+        List<Movie> test =   Movie.initializeMovies();
+        assertNotNull(test);
+
+
+          }
 
 }
