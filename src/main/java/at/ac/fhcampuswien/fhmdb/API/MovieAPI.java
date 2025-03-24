9bb653c940abd6a0e3d26e87600f5124b8b1c0b4
Florian Koller <koller.florian@gmail.com>
2025-03-24 18:55:46 +0100

    Added dependencies and a rutimentary version of API

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/API/MovieAPI.java
@@ -0,0 +1,31 @@
+package at.ac.fhcampuswien.fhmdb.API;
+
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import okhttp3.*;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class MovieAPI {
+    final OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        MovieAPI example = new MovieAPI();
+        String response = example.run("https://google.com");
+        System.out.println(response);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,11 @@ module at.ac.fhcampuswien.fhmdb {
     requires javafx.fxml;
 
     requires com.jfoenix;
+    requires jdk.httpserver;
+    requires okhttp3;
+    requires junit;
+    requires hamcrest.core;
+
 
     opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
     exports at.ac.fhcampuswien.fhmdb;
