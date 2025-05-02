87d46969b0388d33c0e957e2684dacf8e69fb34e
Florian Koller <koller.florian@gmail.com>
2025-05-02 10:55:36 +0200

    added a new ui class for watchlist

--- src/main/java/at/ac/fhcampuswien/fhmdb/Controller/ControllerBaseWatchlist.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/Controller/ControllerBaseWatchlist.java
@@ -1,9 +1,25 @@
 package at.ac.fhcampuswien.fhmdb.Controller;
 
+import at.ac.fhcampuswien.fhmdb.API.MovieAPI;
 import at.ac.fhcampuswien.fhmdb.models.Genre;
 import at.ac.fhcampuswien.fhmdb.ui.MovieCell;
+import at.ac.fhcampuswien.fhmdb.ui.MovieCellWatchlist;
+
+import java.io.IOException;
 
 public class ControllerBaseWatchlist extends ControllerBase{
-    public void initializeUI(){}
+    public void initializeUI(){
+        movieListView.setItems(observableMovies);   // set data of observable list to list view
+        movieListView.setCellFactory(movieListView -> new MovieCellWatchlist()); // use custom cell factory to display data
+        observableMovies.clear();
+        MovieAPI api = new MovieAPI();
+        try {
+            observableMovies.addAll(api.getMovies("Dark Knight", "ACTION", 2008, 0));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 
 }
---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCellWatchlist.java
@@ -0,0 +1,100 @@
+package at.ac.fhcampuswien.fhmdb.ui;
+
+import at.ac.fhcampuswien.fhmdb.ClickEventHandler;
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+public class MovieCellWatchlist extends ListCell<Movie> {
+    private final Label title = new Label();
+    private final Label detail = new Label();
+    private final Label genres = new Label();
+    private final Label releaseyear = new Label();
+    private final Label rating = new Label();
+    private final Button watchlist = new Button("Remove from Watchlist");
+    private final VBox layout = new VBox(title, detail, genres,releaseyear,rating, watchlist);
+
+    public MovieCellWatchlist() {}
+    public MovieCellWatchlist(ClickEventHandler removefromWatchlist) {
+        super();
+        watchlist.setOnMouseClicked(mouseEvent -> {
+            removefromWatchlist.onClick(getItem());
+        });
+    }
+
+    @Override
+    protected void updateItem(Movie movie, boolean empty) {
+        super.updateItem(movie, empty);
+
+        if (empty || movie == null) {
+            setGraphic(null);
+            setText(null);
+        } else {
+            this.getStyleClass().add("movie-cell");
+
+            title.setText(movie.getTitle());
+
+            detail.setText(
+                    movie.getDescription() != null
+                            ? movie.getDescription()
+                            : "No description available"
+            );
+
+
+
+            genres.setText(
+                    !movie.getGenres().isEmpty()
+                    ?       movie.getGenres().toString().replace("[", "").replace("]", "")
+                    :       "No genres available"
+            );
+
+            releaseyear.setText(
+                    movie.getReleaseYear() != 0
+                            ?       "Releaseyear: " + movie.getReleaseYear()
+                            :       "No Releaseyear available"
+            );
+            rating.setText(
+                    movie.getRating() != 0
+                            ?       "Rating: " + movie.getRating()
+                            :       "No Rating available"
+            );
+
+
+
+            // color scheme
+            title.getStyleClass().add("text-yellow");
+            detail.getStyleClass().add("text-white");
+            genres.getStyleClass().add("text-white");
+            releaseyear.getStyleClass().add("text-white");
+            rating.getStyleClass().add("text-white");
+            watchlist.getStyleClass().add("background-yellow");
+            layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
+
+            // layout
+            title.fontProperty().set(title.getFont().font(20));
+
+            detail.setMaxWidth(this.getScene().getWidth() - 30);
+            detail.setWrapText(true);
+
+            genres.setMaxWidth(this.getScene().getWidth() - 30);
+            genres.setWrapText(true);
+
+            releaseyear.setMaxWidth(this.getScene().getWidth() - 30);
+            releaseyear.setWrapText(true);
+            rating.setMaxWidth(this.getScene().getWidth() - 30);
+            rating.setWrapText(true);
+
+            layout.setPadding(new Insets(10));
+            layout.spacingProperty().set(10);
+            layout.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
+            setGraphic(layout);
+        }
+    }
+}
+
